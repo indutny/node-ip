a55f30900e20a11827463a3b4f2165381ba5d413
Ben Kelly <ben@wanderview.com>
2013-02-18 16:52:08 -0500

    Support reading and writing Buffers in-place.
    Optionally allow the caller to provide an existing Buffer and offset
    to toString() and toBuffer().  If these arguments are left off, then
    act as the API did previously either creating the Buffer in toBuffer()
    or assuming an offset of zero in toString().
    
    The goal of these changes is to reduce object creation by avoiding
    unnecessary slice() calls.
    
    Tests included.

--- test/api-test.js
+++ test/api-test.js
@@ -11,12 +11,32 @@ describe('IP library for node.js', function() {
       assert.equal(ip.toString(buf), '127.0.0.1');
     });
 
+    it('should convert to buffer IPv4 address in-place', function() {
+      var buf = new Buffer(128);
+      var offset = 64;
+      ip.toBuffer('127.0.0.1', buf, offset);
+      assert.equal(buf.toString('hex', offset, offset + 4), '7f000001');
+      assert.equal(ip.toString(buf, offset, 4), '127.0.0.1');
+    });
+
     it('should convert to buffer IPv6 address', function() {
       var buf = ip.toBuffer('::1');
       assert(/(00){15,15}01/.test(buf.toString('hex')));
       assert.equal(ip.toString(buf), '::1');
       assert.equal(ip.toString(ip.toBuffer('1::')), '1::');
       assert.equal(ip.toString(ip.toBuffer('abcd::dcba')), 'abcd::dcba');
+    });
+
+    it('should convert to buffer IPv6 address in-place', function() {
+      var buf = new Buffer(128);
+      var offset = 64;
+      ip.toBuffer('::1', buf, offset);
+      assert(/(00){15,15}01/.test(buf.toString('hex', offset, offset + 16)));
+      assert.equal(ip.toString(buf, offset, 16), '::1');
+      assert.equal(ip.toString(ip.toBuffer('1::', buf, offset),
+                               offset, 16), '1::');
+      assert.equal(ip.toString(ip.toBuffer('abcd::dcba', buf, offset),
+                               offset, 16), 'abcd::dcba');
     });
   });
 
