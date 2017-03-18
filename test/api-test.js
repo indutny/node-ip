f50f9f203f393ef23515a3c728de609e292a5c68
Bearice Ren <bearice@icybear.net>
2017-03-18 11:12:32 +0800

    add toReverseDNS method

--- test/api-test.js
+++ test/api-test.js
@@ -403,5 +403,16 @@ describe('IP library for node.js', function() {
       assert.equal(ip.fromLong(2130706433), '127.0.0.1');
       assert.equal(ip.fromLong(4294967295), '255.255.255.255');
     });
-  })
+  });
+
+  describe('toReverseDNS() method', function () {
+    it('should repond with ipv4 address', function () {
+      assert.equal(ip.toReverseDNS('127.0.0.1'), '1.0.0.127.in-addr.arpa');
+      assert.equal(ip.toReverseDNS('1.2.3.4'), '4.3.2.1.in-addr.arpa');
+    });
+    it('should repond with ipv6 address', function () {
+      assert.equal(ip.toReverseDNS('::ffff:127.0.0.1'), '1.0.0.0.0.0.f.7.f.f.f.f.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa');
+      assert.equal(ip.toReverseDNS('2001:db8::567:89ab'), 'b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa');
+    });
+  });
 });
