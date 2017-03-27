b085372c89c21805c49657715bc48b93488e23e5
Bearice Ren <bearice@icybear.net>
2017-03-27 17:54:06 +0800

    passed jslint check

--- test/api-test.js
+++ test/api-test.js
@@ -411,8 +411,12 @@ describe('IP library for node.js', function() {
       assert.equal(ip.toReverseDNS('1.2.3.4'), '4.3.2.1.in-addr.arpa');
     });
     it('should repond with ipv6 address', function () {
-      assert.equal(ip.toReverseDNS('::ffff:127.0.0.1'), '1.0.0.0.0.0.f.7.f.f.f.f.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa');
-      assert.equal(ip.toReverseDNS('2001:db8::567:89ab'), 'b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa');
+      var r1;
+      var r2;
+      r1 = '1.0.0.0.0.0.f.7.f.f.f.f.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0';
+      assert.equal(ip.toReverseDNS('::ffff:127.0.0.1'), r1 + '.ip6.arpa');
+      r2 = 'b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2';
+      assert.equal(ip.toReverseDNS('2001:db8::567:89ab'), r2 + '.ip6.arpa');
     });
   });
 });
